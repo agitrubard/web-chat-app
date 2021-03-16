95c32e3667d162b4e2af5430ed07e05b2dd756b2
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-03-16 09:28:26 +0300

    Web Chat Application

---
+++ src/main/java/com/agitrubard/webchatapp/WebChatApplication.java
@@ -0,0 +1,11 @@
+package com.agitrubard.webchatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebChatApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WebChatApplication.class, args);
+	}
+}
