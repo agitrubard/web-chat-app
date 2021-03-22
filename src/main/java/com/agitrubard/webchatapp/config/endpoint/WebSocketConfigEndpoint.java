0197abdcd90d4c2ba78836f44c4ac48440c5ddc5
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-03-22 19:16:38 +0300

    WebSocket Configuration Added

---
+++ src/main/java/com/agitrubard/webchatapp/config/endpoint/WebSocketConfigEndpoint.java
@@ -0,0 +1,8 @@
+package com.agitrubard.webchatapp.config.endpoint;
+
+public class WebSocketConfigEndpoint {
+
+    public static final String WS = "/ws";
+    public static final String APP = "/app";
+    public static final String TOPIC = "/topic";
+}
