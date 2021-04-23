e76c66bbddf8ce5b36d5e83c704fa31d195eaea8
Agit Rubar Demir <demiragitrubar@gmail.com>
2021-04-23 20:41:27 +0300

    ApplicationStartupService Created For Show Info Message When Application Started

---
+++ src/main/java/com/agitrubard/webchatapp/service/ApplicationStartupService.java
@@ -0,0 +1,15 @@
+package com.agitrubard.webchatapp.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class ApplicationStartupService implements ApplicationListener<ApplicationReadyEvent> {
+    @Override
+    public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        log.info("WEB CHAT APP STARTED!");
+    }
+}
